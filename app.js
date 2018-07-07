54c4722c636b9d24f84ed960d5b886073a5a5e9f
Palaz <zoltan.palatinus@inteliart.hu>
2018-07-07 10:03:41 +0200

    get and populate items

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ const ItemCtrl = (function(){
     // Data Structure / State
 
     const data = {
-        items = [
+        items : [
             {id: 0, name: 'Steak Dinner', calories: 1200},
             {id: 1, name: 'Cookie', calories: 400},
-            {id: 2, name: 'Eggs', calories: 300},
+            {id: 2, name: 'Eggs', calories: 300}
         ],
         currentItem: null,
         totalCalories: 0
@@ -24,6 +24,9 @@ const ItemCtrl = (function(){
 
     // Public methods
     return {
+        getItems: function() {
+            return data.items;
+        },
         logData: function() {
             return data;
         }
@@ -36,9 +39,27 @@ const ItemCtrl = (function(){
 
 const UICtrl = (function(){
 
+    const UISelectors = {
+        itemList: '#item-list'
+    }
+
     // Public methods
     return {
+        populateItemList: function(items){
+            let html = '';
 
+            items.forEach(function(item){
+                html += `<li class="collection-item" id="item-${item.id}">
+                <strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+                <a href="#" class="secondary-content">
+                  <i class="edit-item fa fa-pencil"></i>
+                </a>
+              </li>`;
+            });
+
+            // Insert list items
+            document.querySelector(UISelectors.itemList).innerHTML = html;
+        }
     }
 })();
 
@@ -52,6 +73,12 @@ const App = (function(ItemCtrl, UICtrl){
     return {
         init: function(){
             console.log('Inititalizing app...');
+            
+            // Ftech items from data structure
+            const items = ItemCtrl.getItems();
+
+            // Populate list with items
+            UICtrl.populateItemList(items);
         }
     }
 
