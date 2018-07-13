e9615b8e8e9baa37f04bd8721f4f74fcaf04010c
Palaz <zoltan.palatinus@inteliart.hu>
2018-07-13 17:49:32 +0200

    add items to the UI

--- app.js
+++ app.js
@@ -13,9 +13,9 @@ const ItemCtrl = (function(){
 
     const data = {
         items : [
-            {id: 0, name: 'Steak Dinner', calories: 1200},
-            {id: 1, name: 'Cookie', calories: 400},
-            {id: 2, name: 'Eggs', calories: 300}
+         //  {id: 0, name: 'Steak Dinner', calories: 1200},
+         //  {id: 1, name: 'Cookie', calories: 400},
+         //  {id: 2, name: 'Eggs', calories: 300}
         ],
         currentItem: null,
         totalCalories: 0
@@ -91,7 +91,30 @@ const UICtrl = (function(){
                 calories: document.querySelector(UISelectors.itemCaloriesInput).value
             }
         },
-
+        addListItem: function(item){
+            // Show the list
+            document.querySelector(UISelectors.itemList).style.display = 'block';
+            // Create li element
+            const li = document.createElement('li');
+            // Add class
+            li.className = 'collection-item';
+            // Add ID
+            li.id = `item-${item.id}`;
+            // Add Html
+            li.innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fa fa-pencil"></i>
+            </a>`;
+            // Insert item
+            document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
+        },
+        clearInput: function() {
+            document.querySelector(UISelectors.itemNameInput).value = '';
+            document.querySelector(UISelectors.itemCaloriesInput).value = '';
+        },
+        hideList: function(){
+            document.querySelector(UISelectors.itemList).style.display = 'none';
+        },
         getSelectors: function(){
             return UISelectors;
         }
@@ -121,6 +144,11 @@ const App = (function(ItemCtrl, UICtrl){
         if(input.name !== '' && input.calories !== '') {
             // Add item
             const newItem = ItemCtrl.addItem(input.name, input.calories);
+            // Add item to UI List
+            UICtrl.addListItem(newItem);
+
+            // Clear fields
+            UICtrl.clearInput();
         }
 
         e.preventDefault();
@@ -134,8 +162,13 @@ const App = (function(ItemCtrl, UICtrl){
             // Ftech items from data structure
             const items = ItemCtrl.getItems();
 
-            // Populate list with items
-            UICtrl.populateItemList(items);
+            // Check if any items
+            if(items.length === 0) {
+                UICtrl.hideList();
+            }else {
+                // Populate list with items
+                UICtrl.populateItemList(items);
+            }
 
             // Load event listeners
             loadEventListeners();
